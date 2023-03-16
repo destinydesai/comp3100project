8ff67ad9a8ad4f429a19097f0a077d973f4274dd
destinydesai <100756652+destinydesai@users.noreply.github.com>
2023-03-16 18:16:02 +1100

    Add files via upload

---
+++ MyServer.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.net.*;
+
+public class MyServer {
+  public static void main(String[] args) throws Exception {
+    ServerSocket ss = new ServerSocket(5000);
+    Socket s = ss.accept();
+    DataInputStream dis = new DataInputStream(s.getInputStream());
+    DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+    BufferedReader in = new BufferedReader (new InputStreamReader(s.getInputStream()));
+  
+
+    String str = in.readLine();
+    System.out.println("message = " + str);
+    dout.write(("OK\n").getBytes());
+    dout.flush();
+    String str1 = in.readLine();
+    System.out.println("message = " + str1);
+    dout.write(("OK\n").getBytes());
+    dout.flush();
+    dout.close();
+    s.close();
+    ss.close();
+    }
+}
